fa2ad3006610eff062c1680068691264a87ac0c9
anthony soon <anthonychansoon@gmail.com>
2020-06-23 14:47:18 -0700

    fix object

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ 
+    extended: true 
+}));
 
 app.use(express.static("public"));
 
